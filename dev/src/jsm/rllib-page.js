74a1844086ffcbecfb41d9082364a230e4e9ae6d
polyzer <realpolyzer@yandex.ru>
2020-10-17 04:48:14 +0500

    update

---
+++ dev/src/jsm/rllib-page.js
@@ -0,0 +1,12 @@
+
+import {CreditsInfoBox} from './CreditsInfoBox';
+class Program {
+  constructor() {
+  }
+
+}
+
+let ProgramObject = new Program();
+window.addEventListener("load", function(){
+  let creds = new CreditsInfoBox();
+})
